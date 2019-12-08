f4571cf04f78333da84c34c2fc9c7d33915347c8
jagreehal <jag.reehal@gmail.com>
2019-12-08 23:22:30 +0000

    now copies stencil components

--- packages/eleventy-site/.eleventy.js
+++ packages/eleventy-site/.eleventy.js
@@ -1,17 +1,22 @@
-const htmlmin = require("html-minifier");
+// const htmlmin = require("html-minifier");
 const { renderToString } = require("stencil-web-components/hydrate");
 module.exports = function(config) {
   config.addPassthroughCopy("static");
+  config.addPassthroughCopy({
+    "node_modules/stencil-web-components/dist/stencil-web-components":
+      "static/stencil-web-components"
+  });
 
   config.addTransform("ssr", async (content, outputPath) => {
     if (outputPath.endsWith(".html")) {
       try {
         const { html } = await renderToString(content);
-        return htmlmin.minify(html, {
-          useShortDoctype: true,
-          removeComments: false,
-          collapseWhitespace: true
-        });
+        return html;
+        // return htmlmin.minify(html, {
+        //   useShortDoctype: true,
+        //   removeComments: false,
+        //   collapseWhitespace: true
+        // });
       } catch (error) {
         return error;
       }
